70dc508fd60c5831fce450f0e0326697b4a17e38
Anurag Roy <anuragroy485@gmail.com>
2025-04-14 11:06:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll effect for navigation
+document.querySelectorAll(".navbar a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
